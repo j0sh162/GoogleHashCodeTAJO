3ce140a4ee50a46adb111416f1d17d60086301da
j0sh162 <joshkramer567@gmail.com>
2022-02-24 19:23:34 +0100

    person

--- Person.java
+++ Person.java
@@ -1,5 +1,19 @@
 public class Person {
     private String name;
-    private String skill;
+    private String[] skill;
     private boolean used = false;
+    public Person(String n, String[] s){
+        name = n;
+        skill = s;
+        
+    }
+    public String[] getSkill(){
+        return skill;
+    }
+    public void setUsed(boolean u){
+        used = u;
+    }
+    public boolean getUsed(){
+        return used;
+    }
 }
